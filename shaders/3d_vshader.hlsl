9f15ba3f83c3516b3ea888c9c5e9a4929757a72e
boquidev <gd.boqui@gmail.com>
2024-04-05 17:31:15 -0500

    i don't see anything in my calend app so i am trying to debug it

--- shaders/3d_vshader.hlsl
+++ shaders/3d_vshader.hlsl
@@ -6,7 +6,7 @@ VS_OUTPUT_DEFAULT vs( VS_INPUT_DEFAULT input )
 
 	float4 vertex_world_pos = mul(object_transform, float4(input.vertex_pos, 1.0f) );
 	result.vertex_world_pos = vertex_world_pos.xyz;
-	result.pixel_pos = mul( projection_view_matrix, vertex_world_pos);
+	result.pixel_pos = mul( projection_matrix,mul(world_view_matrix, vertex_world_pos));
 
 	result.texcoord.x = object_texrect.x + (input.texcoord.x * object_texrect.z);
 	result.texcoord.y = object_texrect.y + (input.texcoord.y * object_texrect.w);
--- shaders/simple_pshader.hlsl
+++ shaders/simple_pshader.hlsl
@@ -4,7 +4,7 @@ float4 ps(VS_OUTPUT_DEFAULT input, uint tid : SV_PrimitiveID) : SV_TARGET
 {
 	float4 texcolor = color_texture.Sample( sampler0, input.texcoord );
 
-	clip(texcolor.a *input.color.a -0.1);
+	// clip(texcolor.a *input.color.a -0.1);
 
 	float4 result = float4(
 		texcolor.r * input.color.r, 
